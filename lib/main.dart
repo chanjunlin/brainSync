cdc1939b9f00420fd601955580148c435293b836
chanjunlin <junlinchan0001@gmail.com>
2024-05-24 14:37:24 +0800

    better working login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,50 @@
+import 'package:brainsync/pages/login.dart';
+import 'package:brainsync/services/navigation_service.dart';
+import 'package:brainsync/utils.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:brainsync/widget_tree.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 Future<void> main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  // runApp(const MyApp());
+  await setup();
+  runApp(MyApp()
+  );
+}
+
+Future<void> setup() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-  runApp(const MyApp());
+  await setUpFireBase();
+  await registerServices();
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GetIt _getIt = GetIt.instance;
+  late NavigationService _navigationService;
 
-  // This widget is the root of your application.
+  MyApp({super.key}) {
+    _navigationService = _getIt.get<NavigationService>();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _navigationService.navigatorKey,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+        textTheme: GoogleFonts.montserratTextTheme(),
       ),
-      home: const WidgetTree(),
+      initialRoute: "/login",
+      routes: _navigationService.routes,
     );
   }
 }
---
+++ lib/utils.dart
@@ -0,0 +1,23 @@
+import 'package:brainsync/firebase_options.dart';
+import 'package:brainsync/services/auth_service.dart';
+import 'package:brainsync/services/navigation_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get_it/get_it.dart';
+
+Future<void> setUpFireBase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
+
+Future<void> registerServices() async {
+  final GetIt getIt = GetIt.instance;
+  getIt.registerSingleton<AuthService>(
+    AuthService(),
+  );
+  getIt.registerSingleton<NavigationService>(
+    NavigationService(),
+  );
+}
+
