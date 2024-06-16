1eb535f063e6f25a79b2f4d340c3e273a6883d6d
junlin <117089326+chanjunlin@users.noreply.github.com>
2024-06-16 16:59:52 +0800

    merged master and updatedChat, all is working

--- lib/pages/Administation/login.dart
+++ lib/pages/Administation/login.dart
@@ -1,15 +1,7 @@
-import 'package:brainsync/auth.dart';
 import 'package:brainsync/pages/form/login_form.dart';
 import 'package:brainsync/services/navigation_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-
-import '../../common_widgets/custom_form_field.dart';
-import '../../const.dart';
-import '../../services/alert_service.dart';
-import '../../services/auth_service.dart';
-import 'forget_password.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -22,27 +14,13 @@ class _LoginPageState extends State<LoginPage> {
   bool _isLoading = false;
 
   final GetIt _getIt = GetIt.instance;
-  final GlobalKey<FormState> _loginFormKey = GlobalKey();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  User? _user;
-
 
   late NavigationService _navigationService;
-  late AlertService _alertService;
-  late AuthService _authService;
 
   @override
   void initState() {
     super.initState();
     _navigationService = _getIt.get<NavigationService>();
-    _alertService = _getIt.get<AlertService>();
-    _authService = _getIt.get<AuthService>();
-    _auth.authStateChanges().listen((event) {
-      setState(() {
-        _user = event;
-      });
-    });
   }
 
   @override
@@ -114,141 +92,6 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget createAnAccount() {
-          const SizedBox(height: 10),
-          return Container(
-            width: double.infinity,  // Matches the width of the form fields
-            child: Image.asset(
-              "assets/img/study.png",
-              alignment: Alignment.topCenter,
-              fit: BoxFit.fitWidth, // Ensure the image fits within the container width
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _loginForm() {
-    return Form(
-        key: _loginFormKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomFormField(
-              labelText: "Email",
-              hintText: "Enter a valid email",
-              height: MediaQuery.sizeOf(context).height * 0.09,
-              validationRegEx: EMAIL_VALIDATION_REGEX,
-              onSaved: (value) {
-                setState(() {
-                  email = value;
-                });
-              },
-            ),
-            CustomFormField(
-              labelText: "Password",
-              hintText: "Enter a valid password",
-              obscureText: true,
-              height: MediaQuery.sizeOf(context).height * 0.09,
-              validationRegEx: PASSWORD_VALIDATION_REGEX,
-              onSaved: (value) {
-                setState(() {
-                  password = value;
-                });
-              },
-            ),
-            _forgetPassword(),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.brown[300],
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                ),
-                onPressed: () async {
-                  if (_loginFormKey.currentState?.validate() ?? false) {
-                    _loginFormKey.currentState?.save();
-                    bool result = await _authService.login(email!, password!);
-                    if (result) {
-                      _navigationService.pushReplacementName("/home");
-                    } else {
-                      _alertService.showToast(
-                        text: "Invalid email or password! (Checked if you have verified your email)",
-                        icon: Icons.error_outline_rounded,
-                      );
-                    }
-                  }
-                },
-                child: const Text(
-                  "Login",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Center(
-              child: Text("----------or sign in with----------", style: TextStyle(
-                fontSize: 12,
-                color: Color.fromARGB(255, 78, 52, 46),
-              ),
-              ),
-            ),
-            _googlebutton(),
-          ],
-        ),
-      );
-  }
-
-  Widget _forgetPassword() {
-    return Center(
-      child: TextButton(
-        child: Text(
-          "Forget Your Password?",
-          style: TextStyle(
-            decoration: TextDecoration.underline,
-            color: Colors.brown.shade800,
-            fontSize: 12,
-          ),
-        ),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const ForgetPassword(),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _googlebutton() {
-    return Center(child: SizedBox(
-      height: 50,
-      child: SignInButton(Buttons.google,
-      text: "google",
-      onPressed: _googlesignin,
-      ),
-    ),);
-  }
-
-  void _googlesignin() {
-    try {
-      GoogleAuthProvider _googleAuthProvider = GoogleAuthProvider();
-      _auth.signInWithProvider(_googleAuthProvider);
-    }
-    catch (error) {
-      print(error);
-    }
-  }
-
-  Widget _createAnAccount() {
     return Row(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -280,5 +123,3 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-
-
