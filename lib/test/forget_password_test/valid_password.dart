3c829de5b27f52568c773e60819b32c5d0df88ef
ev <wuchenfeng0214@gmail.com>
2024-07-01 13:41:35 +0800

    forget_password_test

--- lib/test/forget_password_test/valid_password.dart
+++ lib/test/forget_password_test/valid_password.dart
@@ -1,6 +1,4 @@
 import 'package:brainsync/pages/Administation/forget_password.dart';
-import 'package:brainsync/services/navigation_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -8,17 +6,22 @@ import 'package:get_it/get_it.dart';
 import 'package:mockito/mockito.dart';
 import 'package:brainsync/services/auth_service.dart';
 import 'package:brainsync/services/alert_service.dart';
+import 'package:brainsync/services/navigation_service.dart';
 
 class MockAlertService extends Mock implements AlertService {}
-
-class MockNavigationService extends Mock implements NavigationService {}
 
 class MockAuthService extends Mock implements AuthService {
   @override
   Future<void> sendPasswordResetEmail(String email) async {
-    throw FirebaseAuthException(code: 'invalid-email');
+    return super.noSuchMethod(
+      Invocation.method(#sendPasswordResetEmail, [email]),
+      returnValue: Future.value(),
+      returnValueForMissingStub: Future.value(),
+    );
   }
 }
+
+class MockNavigationService extends Mock implements NavigationService {}
 
 void main() {
   late MockAlertService alertService;
@@ -45,7 +48,10 @@ void main() {
     GetIt.instance.reset();
   });
 
-  testWidgets('invalid password test', (WidgetTester tester) async {
+  testWidgets('valid email test', (WidgetTester tester) async {
+    when(authService.sendPasswordResetEmail('wuchenfeng0214@gmail.com'))
+        .thenAnswer((_) async {});
+
     await tester.pumpWidget(const MaterialApp(
       home: ForgetPassword(),
     ));
@@ -56,31 +62,13 @@ void main() {
     expect(emailField, findsOneWidget);
     expect(button, findsOneWidget);
 
-    await tester.enterText(emailField, 'doesnotexist@gmail.com');
+    await tester.enterText(emailField, 'wuchenfeng0214@gmail.com');
     await tester.tap(button);
     await tester.pumpAndSettle();
 
     verify(alertService.showToast(
-      text: 'Invalid email',
-      icon: Icons.error_outline_rounded,
+      text: "Password reset email sent!",
+      icon: Icons.check,
     )).called(1);
   });
-
-  /*testWidgets('valid email test', (WidgetTester tester) async {
-      await tester.pumpWidget(const MaterialApp(
-        home: ForgetPassword(),
-      ));
-
-      var emailField = find.byType(TextField);
-      var button = find.text('Continue');
-
-      expect(emailField, findsOneWidget);
-      expect(button, findsOneWidget);
-
-      await tester.enterText(emailField, 'wuchenfeng0214@gmail.com');
-      await tester.tap(button);
-      await tester.pumpAndSettle();
-
-      expect(find.text('Password reset email sent!'), findsOneWidget);
-  });*/
 }
