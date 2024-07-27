eeff7f4de049172c3ab970ba348e56841396ac01
junlin <117089326+chanjunlin@users.noreply.github.com>
2024-07-27 20:16:28 +0800

    fixed creating private chat not being able to send messages

--- lib/pages/Chats/private_chat/create_private_chat.dart
+++ lib/pages/Chats/private_chat/create_private_chat.dart
@@ -4,6 +4,7 @@ import 'package:get_it/get_it.dart';
 import '../../../common_widgets/chat_tile.dart';
 import '../../../const.dart';
 import '../../../model/user_profile.dart';
+import '../../../services/auth_service.dart';
 import '../../../services/database_service.dart';
 import '../../../services/navigation_service.dart';
 import 'chat_page.dart';
@@ -17,12 +18,14 @@ class PrivateChat extends StatefulWidget {
 
 class _PrivateChatState extends State<PrivateChat> {
   final GetIt _getIt = GetIt.instance;
+  late AuthService _authService;
   late DatabaseService _databaseService;
   late NavigationService _navigationService;
 
   @override
   void initState() {
     super.initState();
+    _authService = _getIt.get<AuthService>();
     _databaseService = _getIt.get<DatabaseService>();
     _navigationService = _getIt.get<NavigationService>();
   }
@@ -73,7 +76,7 @@ class _PrivateChatState extends State<PrivateChat> {
               itemBuilder: (context, index) {
                 UserProfile? friend = snapshot.data![index];
                 if (friend == null) return const SizedBox.shrink();
-
+                print(friend);
                 return CustomChatTile(
                   leading: CircleAvatar(
                     backgroundImage:
@@ -81,11 +84,21 @@ class _PrivateChatState extends State<PrivateChat> {
                   ),
                   title: "${friend.firstName} ${friend.lastName}",
                   subtitle: friend.bio ?? 'No bio available',
-                  onTap: () {
+                  onTap: () async {
+                    final chatExists = await _databaseService.checkChatExist(
+                        _authService.currentUser!.uid, friend!.uid!);
+                    if (!chatExists) {
+                      await _databaseService.createNewChat(
+                          _authService.currentUser!.uid, friend!.uid!);
+                    }
+                    UserProfile? user =
+                        await _databaseService.fetchUserProfile(friend!.uid!);
                     _navigationService.push(
-                      MaterialPageRoute(builder: (context) {
-                        return ChatPage(chatUser: friend);
-                      }),
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return ChatPage(chatUser: user);
+                        },
+                      ),
                     );
                   },
                 );
